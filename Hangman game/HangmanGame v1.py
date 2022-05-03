81cd98d3a39b1e7613f5eaf0b4e3466c7be0c797
RetrowPoLo <100202418+RetrowPoLo@users.noreply.github.com>
2022-05-03 14:31:29 +0200

    Update HangmanGame v1.py

--- Hangman game/HangmanGame v1.py
+++ Hangman game/HangmanGame v1.py
@@ -20,21 +20,7 @@ while attemp > 0:
         print(" You find a letter ! \n")
     else:
         attemp = attemp - 1
-        print(f" Letter not in the word ! \n")
-        if attemp == 0:
-            print(" ==========Y= ")
-        if attemp <= 1:
-            print(" ||/       |  ")
-        if attemp <= 2:
-            print(" ||        0  ")
-        if attemp <= 3:
-            print(" ||       /|\ ")
-        if attemp <= 4:
-            print(" ||       /|  ")
-        if attemp <= 5:                    
-            print("/||           ")
-        if attemp <= 6:
-            print("==============\n")
+        print(f" Letter not in the word ! {attemp} more attemps \n")
         if attemp == 0:
             print(f" The word to find was '{word}' ")
     
